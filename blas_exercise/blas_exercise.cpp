64368fc0704b735c7045cd62e030b80f8a1694cf
Viswanath Pasumarthi <pasumart@buffalo.edu>
2017-07-28 15:30:09 -0400

    save data to a 2D array

--- blas_exercise/blas_exercise.cpp
+++ blas_exercise/blas_exercise.cpp
@@ -1,17 +1,27 @@
 #include <iostream>
 #include <fstream>
+#include <string>
 
 using namespace std;
 
 int main ()
 {
   string line;
+  double value;
+  int n, m;
   ifstream myfile ("C.data");
   if (myfile.is_open())
   {
+    int len = 7;
+    float data [len][len];
     while ( getline (myfile,line) )
     {
-      cout << line << '\n';
+     for (n=0; n<len; n++)
+         for (m=0; m<len; m++)
+         {
+             double value = std::stod(line);
+             data[n][m] = value;
+         }
     }
     myfile.close();
   }
